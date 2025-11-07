8c447470640c5803228eed3f77f7fbdf56ffde38
Vanessa Sierra <sierrava212@yahoo.com>
2025-11-06 22:48:56 -0500

    added event listeners to js file

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-// Step 1: Select all the elemnts we need
+// Step 1: Select all the elements we need (DOM Selection)
 const form = document.getElementById("registrationForm");
 const usernameInput = document.getElementById("username");
+const emailInput = document.getelementbyId("email");
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirmPassword");
+
+// Error Messages// html ln 26, ln 45 ,ln 61 ln 80 //
+const usernameError = document.getElementById("usernameErrror");
+const emailError = document.getElementById("emailError");
+const passwordError = document.getElementById("passwordError");
+const confirmPasswordInput = document.getElementById("confirmPassword");
+
+
+//if saved, load username from LOCALsTORAGE//
+window.addEventListener("DOMContentLoaded",()=>{
+    const savedUsername = localStorage.getItem("username");
+    if (savedUsername) {
+        usernameInput.value = savedUsername;
+    }
+});
